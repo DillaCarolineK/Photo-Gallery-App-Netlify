4fa8031e197552a035dfa9f0ef5b6ff0b980ab4d
Dilla Caroline Khairunnisa <dillajepara578@students.unnes.ac.id>
2023-06-19 21:42:34 +0700

    Update Photo Gallery App Netlify

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Dilla Caroline Khairunnisa'" // TODO: replace this
+const studentId = "FE4424338" // TODO: replace this
+
+export { url, studentName, studentId }
